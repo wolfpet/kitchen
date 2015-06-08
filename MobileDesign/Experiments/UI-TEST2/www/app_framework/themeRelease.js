9d52a45e96abaff5dc9fc628efd730a669f77ce3
wolfpet <p_wol@hotmail.com>
2015-06-07 21:59:07 -0400

    Mobile UI experiments
    A few experiments with Intel App Framework prior to building the final
    mobile UI

---
+++ MobileDesign/Experiments/UI-TEST2/www/app_framework/themeRelease.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2015 Intel Corporation. All rights reserved.
+ *
+ * App Framework will use a 'native' theme unless told not to.
+ */
+
+(function()
+ {
+  "use strict";
+  var appf = af || $;
+  appf.ui.useOSThemes = false;
+ })();
