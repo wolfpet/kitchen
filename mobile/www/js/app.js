42ec4a6c1a1ceae50b2cb3f565f439ca213b2e37
wolfpet <p_wol@hotmail.com>
2015-06-13 12:53:01 -0400

    Some UI tweaks
    Improved the tree navigation, added some css to the message view area

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -5,20 +5,21 @@ function onAppReady() {
     if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
         navigator.splashscreen.hide() ;
     }
+    loadRootThreads()
 }
 document.addEventListener("app.Ready", onAppReady, false) ;
 
 
 
-function callKitchen(threadId)
+function loadRootThreads()
 {
     var url = mainUrl+ "api/threads";
-    if(null != threadId){url=url+"/"+threadId}
+    //if(null != threadId){url=url+"/"+threadId}
     listStuff = document.getElementById("stuffs_list");
-    var apiCall = $.get(url, function(data) {kitchenCallback(data);}); 
+    var apiCall = $.get(url, function(data) {loadRootThreadsCallback(data);}); 
 }
 
-function kitchenCallback(payload) 
+function loadRootThreadsCallback(payload) 
 {
     //clear the list
     $("#stuffs_list").empty();
@@ -28,15 +29,18 @@ function kitchenCallback(payload)
         var subj = data.threads[i].message.subject;
         //check the number of replies. if >0 then render the badge
         var badgeHtml ="";
+        var repliesLinkHtml="";
+        var readMsgButtonHtml ="<div class='button-grouped'><a class='button' onclick='javascript:displayMessage("+data.threads[i].message.id+")'>Read Message</a>";
         if(data.threads[i].counter > 0)
         {
-            badgeHtml= "<span class='af-badge tl'>"+data.threads[i].counter+"</span>";
+            repliesLinkHtml="<div class='button' onclick='javascript:showReplies("+data.threads[i].message.id+");'><span class='af-badge tl'>"+data.threads[i].counter+"</span>&nbsp;&nbsp;&nbsp;&nbsp;Show replies </div>"
         }
+        readMsgButtonHtml = readMsgButtonHtml + repliesLinkHtml + "</div>";
         //Append the title to the list
         var li = document.createElement('li');
         li.setAttribute('class','widget uib_w_7');
         li.setAttribute('data-uib','app_framework/listitem');
-        li.innerHTML= badgeHtml +  "<a href='#' onclick='javascript:displayMessage("+data.threads[i].message.id+")'>"+subj+"<br /><b>"+data.threads[i].message.author.name+"</b></a><br/><div onclick='javascript:showReplies("+data.threads[i].message.id+");'>See replies </div>   ";                
+        li.innerHTML= "<p onclick='javascript:displayMessage("+data.threads[i].message.id+")'><b>"+data.threads[i].message.author.name+"</b> wrote:<br /><span style='color:#0088d1'>"+subj+"</span><br /></p>"+readMsgButtonHtml;                
         listStuff.appendChild(li);        
     }           
 }
@@ -53,6 +57,8 @@ function showRepliesCallback(payload)
 {
     //clear the list
     $("#stuffs_list").empty();
+    //clear the message div as well
+    document.getElementById('msgBody').innerHTML="";
     var data = $.parseJSON(payload);
     for(i=0; i<data.count; i++)
     {
@@ -60,19 +66,25 @@ function showRepliesCallback(payload)
         //check the number of replies. if >0 then render the badge
         var badgeHtml ="";
         var replyLinkHtml="";
+        var showRepliesHtml="";
+        var readMsgButtonHtml ="<div class='button-grouped'><div class='button' onclick='javascript:displayMessage("+data.messages[i].id+")'>Read Message</div>";
         if(data.messages[i].answers > 0)
         {
             badgeHtml= "<span class='af-badge tl'>"+data.messages[i].answers+"</span>";
-            replyLinkHtml="<br/><p onclick='javascript:showReplies("+data.messages[i].id+");'>See replies&nbsp;&nbsp;&nbsp;</p>"
+            showRepliesHtml="<div class='button' onclick='javascript:showReplies("+data.messages[i].id+");'>"+badgeHtml+">&nbsp;&nbsp;&nbsp;&nbsp;Show Replies</div>";            
         }
-        var parentHTML ="<p onclick='javascript:showReplies("+data.messages[i].parent+");'> Level up</p>"
+        readMsgButtonHtml = readMsgButtonHtml + showRepliesHtml;
+        
+        var parentHTML ="<div class='button' onclick='javascript:showReplies("+data.messages[i].parent+");'>Back</div>"
         //Append the title to the list
         var li = document.createElement('li');
         li.setAttribute('class','widget uib_w_7');
         li.setAttribute('data-uib','app_framework/listitem');
-        li.innerHTML= badgeHtml +  "<a href='#' onclick='javascript:displayMessage("+data.messages[i].id+")'>"+subj+"<br /><b>"+data.messages[i].author.name+"</b></a>" + replyLinkHtml + parentHTML;                
-        listStuff.appendChild(li);        
+        li.innerHTML= "<p onclick='javascript:displayMessage("+data.messages[i].id+")'><b>"+data.messages[i].author.name+"</b> wrote: <br /><span style='color:#0088d1'>"+subj+"</span><br /></p>"+readMsgButtonHtml + parentHTML+"</div>";
+                          
+        listStuff.appendChild(li);                
     }           
+    
 }
 
 
@@ -89,7 +101,7 @@ function displayMessageCallback(payload)
     var msg = data.body.html;
     ///alert(msg);
     //$.ui.popup(msg);
-    document.getElementById('openModal').innerHTML = msg;
+    document.getElementById('msgBody').innerHTML = msg;
     
 }
     
