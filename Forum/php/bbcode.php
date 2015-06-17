2f95b7f399a8651b2c1f12e39d4870ea408400f5
ranger <iamranger@users.noreply.github.com>
2015-06-17 00:56:21 -0700

    bbcode

---
+++ Forum/php/bbcode.php
@@ -0,0 +1,162 @@
+<?php
+
+function do_bbcode($body) {
+  global $user;
+  
+  if(!extension_loaded('fastbbcode') || isset($user) && in_array($user, array('Ranger', 'Pensioner', 'test', 'echo_maker', 'A. Fig Lee', 'leonid', 'Peter (2)'))) {
+    print('<div id="stamp" style="font-size: 8px;color:gray;position:fixed;left: 0px;top: 0px;width: 100%;height: 8px;z-index: 9999;text-align: right;">phpBBCode&nbsp;</div>');
+    return bbcode_format($body);
+  } else 
+    return bbcode($body);
+}
+
+function bbcode_format($str){
+  // Convert all special HTML characters into entities to display literally
+  
+  // The array of regex patterns to look for
+  $format_search = array(
+      '#\[b\](.*?)\[/b\]#is', // Bold ([b]text[/b]
+      '#\[i\](.*?)\[/i\]#is', // Italics ([i]text[/i]
+      '#\[u\](.*?)\[/u\]#is', // Underline ([u]text[/u])
+      '#\[s\](.*?)\[/s\]#is', // Strikethrough ([s]text[/s])
+      '#\[code\](.*?)\[/code\]#is', // Monospaced code [code]text[/code])
+      '#\[size=([1-9]|1[0-9]|20)\](.*?)\[/size\]#is', // Font size 1-20px [size=20]text[/size])
+      '#\[color=([A-F0-9]{3}|[A-F0-9]{6})\](.*?)\[/color\]#is', // Font color ([color=00F]text[/color])
+      '#\[color=(.*?)\](.*?)\[/color\]#is', // Font color ([color=#00F]text[/color]) or Font color ([color={color_name}]text[/color])
+      '#\[url=((?:ftp|https?)://[^\]\s]*)\s*\](.*?)\[/url\]#is', // Hyperlink with descriptive text ([url=http://url]text[/url])
+      '#\[url=([^\]\s]*)\s*\](.*?)\[/url\]#is', // Hyperlink with descriptive text ([url=http://url]text[/url])
+      '#\[url\]((?:ftp|https?)://[^\s<\["]*)\s*\[/url\]#i', // Hyperlink ([url]http://url[/url]),
+      '#\[url\]([^\s<\["]*)\s*\[/url\]#i', // Hyperlink ([url]http://url[/url]) 
+      '#\[img=(https?://\S*?)\s*\]#i', // Image ([img=http://url_to_image[/img])
+      '#\[img=(\S*?)\s*\]#i' // Image ([img=url_to_image[/img])
+  );
+   
+  // The matching array of strings to replace matches with
+  $format_replace = array(
+      '<strong>$1</strong>',
+      '<em>$1</em>',
+      '<span style="text-decoration: underline;">$1</span>',
+      '<span style="text-decoration: line-through;">$1</span>',
+      '<pre>$1</'.'pre>',
+      '<span style="font-size: $1px;">$2</span>',
+      '<span style="color: #$1;">$2</span>',
+      '<span style="color: $1;">$2</span>',
+      '<a target="_blank" href="$1">$2</a>',
+      '<a target="_blank" href="//$1">$2</a>',
+      '<a target="_blank" href="$1">$1</a>',
+      '<a target="_blank" href="//$1">$1</a>',
+      '<img src="$1" alt=""/>',
+      '<img src="//$1" alt=""/>'
+  );
+  
+  // Perform the actual conversion
+  $str = preg_replace($format_search, $format_replace, $str);
+
+  // Deal with quotes
+  $format_search =  array(
+    '#\[quote(?!.*\[quote)\](.*?)\[/quote\]#is', // Quote ([quote]text[/quote])
+	  '#\[quote(?!.*\[quote)=(.*?)\](.*?)\[/quote\]#is', // Quote with author ([quote=author]text[/quote])
+  );
+   
+  // The matching array of strings to replace matches with
+  $format_replace = array(
+    '<blockquote><div>$1</div></blockquote>',
+    '<blockquote><div><cite>$1:</cite>$2</div></blockquote>',
+  );
+   
+  // Perform the actual quotes conversion
+  $count = 1;
+  while ($count > 0) {
+    $str = preg_replace($format_search, $format_replace, $str, -1, $count);
+  }
+  // print('before naked called:-->'.$str.'<--');
+  
+  // Uncoded images & URLs   
+  return bbcode_naked_urls(bbcode_naked_images($str));
+}
+
+// ================ Handling of URLs and Images outside of bb code ==========================
+
+function unless_in_quotes($pattern) {
+return '>'.$pattern.'<\/a>|="'.$pattern.'"|('.$pattern.')';
+}
+
+function bbcode_naked_images($str) {
+  // Deal with untagged images
+  $str = preg_replace_callback('#'.unless_in_quotes('(?:ftp|https?):\/\/[^\s>"]+\.(?:jpg|jpeg|gif|png|bmp)(?:\?[^\s<"]*)?').'#is', // unprocessed images (i.e. without quotes around them)
+    function ($m) {
+      if(empty($m[1])) return $m[0];
+					else return '<img src="' . $m[1] . '" alt=""/>';
+    }, $str);
+  // print("--->".$str."<---");
+  
+  // Google pic URLs are also images
+  $str = preg_replace_callback('#'.unless_in_quotes('https:\/\/[a-z0-9]+\.googleusercontent.com\/[^\s<]+').'#is', // unprocessed images (i.e. without quotes around them)
+    function ($m) {
+      if(empty($m[1])) return $m[0];
+					else return '<img src="' . $m[1] . '" alt=""/>';
+    }, $str);
+  return $str;
+}
+
+function bbcode_naked_urls($str) {
+  return preg_replace_callback('#'.unless_in_quotes('[[:alpha:]]+://[^<>[:space:]]+[[:alnum:]/]').'#is', // unprocessed URLs(i.e. without quotes around them)
+    function ($m) {
+      if(empty($m[1])) return $m[0];
+					else return '<a target="_blank" href="' . $m[1] . '">' . $m[1] . '</a>';
+    }, $str);
+}
+
+/** 
+ * Run this before bbcode is called to render content before bbcode() had a chance to mess it up
+ */
+function before_bbcode($body) {
+  
+  $body = preg_replace( array (
+    // search
+    '#(?<!\[url(=|]))((?:https?://)?(?:www\.)?vimeo\.com/([0-9]*)(?:(?:\?|&)[^\s<\]"]*)?)#is', // Vimeo on-the-fly e.g. https://vimeo.com/129252030
+    '#(?<!\[url(=|]))((?:https?://)?(?:www\.)?coub\.com/(?:view|embed)/([0-9a-zA-Z]*)(?:(?:\?|&)[^\s<\]"]*)?)#is' // Coub on the fly e.g. http://coub.com/view/3lbz7
+    ), array (
+    '<br/><iframe src="https://player.vimeo.com/video/$3" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><br/>Link: <a href="$2">$2</a>',
+    '<br/><iframe src="//coub.com/embed/$3?muted=false&autostart=false&originalSize=false&hideTopBar=false&startWithHD=false" width="500" height="281" frameborder="0" allowfullscreen="true"></iframe><br/>Link: <a href="$2">$2</a>'          
+    ), $body);    
+       
+  return $body;
+}
+
+/** 
+ * Run this after bbcode is called to finalize the rendering of the message body 
+ */
+function after_bbcode($body) {
+  // handle [iframe] tag
+  $body = preg_replace_callback('#\[iframe (.*)\]#i',
+		function ($matches) {
+      return "<iframe ".html_entity_decode($matches[1])."></iframe>";
+    }, $body);
+      
+  $body = preg_replace( array (
+    // search
+    '#\[b\](.*?)\[/b\]#is', // Bold ([b]text[/b]
+    '#\[i\](.*?)\[/i\]#is', // Italics ([i]text[/i]
+    '#\[u\](.*?)\[/u\]#is', // Underline ([u]text[/u])
+    '#\[s\](.*?)\[/s\]#is'  // Strikethrough ([s]text[/s])
+    ), array (
+    // replace
+    '<strong>$1</strong>',
+    '<em>$1</em>',
+    '<span style="text-decoration: underline;">$1</span>',
+    '<span style="text-decoration: line-through;">$1</span>'
+    ), $body);    
+       
+  return fix_msg_target($body);
+}
+
+/** 
+ * Replaces target for URLs that reference messages of this forum
+ */
+function fix_msg_target($body) {
+  global $host;
+  return str_replace('<a target="_blank" href="http://'.$host.'/msg.php?id=', '<a target="bottom" href="http://'.$host.'/msg.php?id=', $body);
+}
+
+?>
