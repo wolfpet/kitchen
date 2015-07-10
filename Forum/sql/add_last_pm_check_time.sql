3af203c3e1bb54010f536d2088b47871850cc108
iamranger <iamranger@users.noreply.github.com>
2015-07-09 18:33:03 -0700

    Update add_last_pm_check_time.sql

--- Forum/sql/add_last_pm_check_time.sql
+++ Forum/sql/add_last_pm_check_time.sql
@@ -1,1 +1,1 @@
-alter table confa_sessions add column last_pm_check_time timestamp null default '0000-00-00 00:00:00';
+alter table confa_users add column last_pm_check_time timestamp null default '0000-00-00 00:00:00';
