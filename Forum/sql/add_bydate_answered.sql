127c8854df78166f3370c0f6174be16a6af0fa3a
iamranger <iamranger@users.noreply.github.com>
2015-07-09 18:32:44 -0700

    Reverting the change - wrong file

--- Forum/sql/add_bydate_answered.sql
+++ Forum/sql/add_bydate_answered.sql
@@ -1,3 +1,2 @@
-alter table confa_users add column last_bydate_time timestamp null default '0000-00-00 00:00:00';
-
-
+alter table confa_sessions add column last_bydate_time timestamp null default '0000-00-00 00:00:00';
+alter table confa_sessions add column last_answered_time timestamp null default '0000-00-00 00:00:00';
