bee22c66baa15b423475e4d2677088483d9e9fea
iamranger <iamranger@users.noreply.github.com>
2015-07-31 12:57:30 -0700

    Added code syntax highlighting to PM

--- Forum/php/pm_msg.php
+++ Forum/php/pm_msg.php
@@ -6,7 +6,8 @@ require_once('get_params_inc.php');
 require_once('html_head_inc.php');
 
 ?>
-
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/styles/default.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js"></script>
 <base target="bottom">
 </head>
 <body style="background-color: #CCEEEE;">
@@ -17,16 +18,9 @@ require("pm_msg_inc.php");
 
 ?>
 
-
-
-
 <!--<a href="">Reply</a> |-->
 
-
 <a href="<?php print($root_dir . $page_pmail_send . '?to=' . $author . '&pm_id=' . $msg_id); ?>">Reply to sender (private)</a> |
-
-
-
 
 </body>
 </html>
