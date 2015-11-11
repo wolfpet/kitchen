92e0368530e5c95cb107671c3ed1b12d3b64e78a
ranger <iamranger@users.noreply.github.com>
2015-11-11 01:32:33 -0800

    Addressed directory traversal vulnerability

--- Forum/php/login.php
+++ Forum/php/login.php
@@ -2,6 +2,7 @@
 /*$Id: login.php 379 2009-11-02 19:40:34Z dmitriy $*/
 
   require_once('login_inc.php');
+  $lastpage = basename($lastpage);
   header("Location: $lastpage", TRUE, 302);
   require_once($lastpage);
 ?>
