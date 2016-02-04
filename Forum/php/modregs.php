eeb674eb1545dbb06d62ffda2504e5c4fc4d1184
ranger <iamranger@users.noreply.github.com>
2016-02-04 03:23:05 -0800

    Added user email address to the list of pending registrations

--- Forum/php/modregs.php
+++ Forum/php/modregs.php
@@ -11,7 +11,7 @@ require_once('head_inc.php');
         $last_id = 0;
         $limit = '';
 
-        $query = 'SELECT username, actkey, CONVERT_TZ(created, \'' . $server_tz . '\', \'' . $prop_tz . ':00\') as created from confa_regs order by username'; 
+        $query = 'SELECT username, email, actkey, CONVERT_TZ(created, \'' . $server_tz . '\', \'' . $prop_tz . ':00\') as created from confa_regs order by username'; 
         $result = mysql_query($query);
         if (!$result) {
             mysql_log(__FILE__, 'query failed ' . mysql_error() . ' QUERY: ' . $query);
@@ -23,8 +23,9 @@ require_once('head_inc.php');
         while ($row = mysql_fetch_assoc($result)) {
             $created = $row['created'];
             $enc_user = htmlentities($row['username'], HTML_ENTITIES,'UTF-8');
+            $enc_mail = htmlentities($row['email'], HTML_ENTITIES,'UTF-8');
             $md5 = $row['actkey'];
-            $line = '<tr><td align="center">'. $enc_user . '</td><td align="center">' . $created . '</td><td width="25%" align="center" nowrap>'
+            $line = '<tr><td align="center">'. $enc_user . '</td><td align="center">'. $enc_mail . '</td><td align="center">' . $created . '</td><td width="25%" align="center" nowrap>'
               .'<form method="get" action="http://'. $host . $root_dir . $page_activate .'" target="bottom">'
               .'<input type="hidden" name="act_link" value="'. $md5.'"/><input name="action" type="submit" value="Confirm"/><input name="action" type="submit" value="Decline"/></form>'
               .'</td></tr>';
@@ -59,7 +60,7 @@ require('menu_inc.php');
 
 <!--<ol>-->
 <table width="95%">
-<tr><th>Username</th><th>Created</th><th>Action</th></tr>
+<tr><th>Username</th><th>Email</th><th>Created</th><th>Action</th></tr>
 <?php print($out); ?>
 </table>
 <!--</ol>-->
