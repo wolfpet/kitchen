efc0a28a83bbf9c0a31dc4cbbbbc87494bac0a9c
iamranger <iamranger@users.noreply.github.com>
2016-09-18 03:21:08 -0700

    ENH:New menu

--- Forum/php/menu_inc.php
+++ Forum/php/menu_inc.php
@@ -3,4 +3,5 @@ notify_about_new_pm($user_id, $last_pm_check_time);
 
 if (!isset($menu_style) || !file_exists('menu'.$menu_style.'.php')) $menu_style=0;
 
+require_once('menu_'.$menu_style.'.php');
 ?>
