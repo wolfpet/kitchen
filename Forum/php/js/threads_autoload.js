2974de193ef75c07ff457688f288470ad485fa02
wolfpet <p_wol@hotmail.com>
2016-10-11 00:16:33 +0000

    adding new scripts for frameless

---
+++ Forum/php/js/threads_autoload.js
@@ -0,0 +1,152 @@
+var selected_id = "";
+
+function selectMsg(id) {
+  id = "sp_" + id;
+  console.log('selectMsg  id=' + id);
+  if (selected_id != "") {
+    // reset selection
+    console.log('resetting selected id=' + selected_id);
+    var selected = document.getElementById(selected_id);
+    if (selected != null) {
+      selected.className = null;
+    }  
+  }
+  var selected = document.getElementById(id);
+  if (selected != null) {
+    console.log('selected element id=' + id);
+    // select message
+    selected.className = "selected";
+    selected_id = id;
+  } else {
+    // message not found
+    selected_id = "";
+    console.log('id=' + id + "not found");
+  }
+}
+
+var bydate_timer = -1;
+var bydate_count = 0;
+
+$( document ).ready(function() {
+  var bydate = document.getElementById('bydate');
+  
+  var newPostsBadge = null;
+
+  if (bydate == null) {
+    newPostsBadge = document.getElementById('newPostsBadge');
+  }
+  
+  if (bydate !== null || newPostsBadge != null) {
+    var update_bydate_counter = function() {
+      var url1 = "./api/messages?mode=bydate&format=count_only";
+      console.log("calling bydate("+url1+")");
+      bydate_count++;
+      var start_time = new Date();
+      $.ajax({
+             type: "GET",
+             url: url1,
+             success: function(obj1) {
+                var end_time = new Date();
+                console.log("bydate object=" + obj1);
+                var count = obj1.count;
+                console.log("bydate=" + count);                
+                if (bydate != null) {
+                  bydate.innerHTML = addCounter(bydate.innerHTML, count, true, false);
+                } else if (newPostsBadge != null) {
+                  if (count > 0) {
+                    newPostsBadge.innerHTML = count;
+                    newPostsBadge.style.display = 'block';
+                  } else {
+                    newPostsBadge.style.display = 'hidden';
+                  }
+                }
+                // update answered badge or element
+                var newAnswersBadge = document.getElementById('newAnswersBadge');
+                var answered = null;
+                if (newAnswersBadge == null) {
+                  answered = document.getElementById('answered');
+                }
+                if (newAnswersBadge != null || answered != null) {
+                  $.ajax({
+                    type: "GET",
+                    url: "./api/messages?mode=answered&format=count_only",
+                    success: function(obj2) {
+                      var count = obj2.count;
+                      console.log("answered=" + count);
+                      if (newAnswersBadge != null) {
+                        if (count > 0) {
+                          newAnswersBadge.innerHTML = count;
+                          newAnswersBadge.style.display = 'block';
+                        } else {
+                          newAnswersBadge.style.display = 'hidden';
+                        }
+                      } else if (answered != null) {
+                        answered.innerHTML = addCounter(answered.innerHTML, count, true, false);                        
+                      }
+                    }
+                  });                             
+                }                
+                // update title
+                var newTitle = addCounter(window.parent.document.title, count, false, true);
+                console.log(newTitle);
+                window.parent.document.title = newTitle;
+                // call user function, if defined
+                if (typeof onNewMessageCount !== 'undefined') {
+                  console.log('calling user function');
+                  onNewMessageCount(count, end_time.getTime() - start_time.getTime());
+                }
+                // adjust frequency of calls if necessary
+                if (bydate_count == 15) {
+                  window.clearInterval(bydate_timer);
+                  bydate_timer = window.setInterval(function() {update_bydate_counter();}, 5*60000);                   
+                  console.log('Checking bydate every 5 min');
+                } else if (bydate_count == 35) {
+                  window.clearInterval(bydate_timer);
+                  bydate_timer = window.setInterval(function() {update_bydate_counter();}, 15*60000);                   
+                  console.log('Checking bydate every 15 min');
+                }                   
+             }
+           });      
+    };
+    window.setTimeout( function() {update_bydate_counter();}, 1000 );
+    bydate_timer = window.setInterval(function(){update_bydate_counter();}, 60000); 
+    console.log('Checking bydate every minute');
+  }
+});
+
+function addCounter(text, count, bold, pad) {
+    var prefix = pad ? " (" : "(";
+    var braket = text.indexOf(prefix);
+    if (braket >= 0) text = text.substring(0, braket);
+    if (count > 0) text += prefix + (bold?"<b>":"") + count + (bold?"</b>":"") +")";
+    return text;                 
+}
+
+var focused = null;
+// shift - select
+$(document).ready(function(){
+ // add click function to checkboxes
+ $(document).find(':checkbox').each(function() {
+    $(this).click(function(e) {
+      if (e.shiftKey) {
+        if (focused != null) {
+          var checked = this.checked;
+          var current = this;
+          // make all checkboxes between 'focused' and 'current' same as 'current'
+          var inside = false;
+          $(document).find(':checkbox').each(function() {
+            if (this.value == current.value || this.value == focused.value) {
+              this.checked = checked;
+              inside = !inside;
+              if (!inside) return;
+            } else if (inside) {
+              this.checked = checked;
+            }
+          });
+        }
+      } else {
+        focused = this;
+      }
+    });
+  });
+});
---
+++ Forum/php/threads.php
@@ -0,0 +1,148 @@
+<?php
+/*$Id: top.php 378 2009-11-02 19:36:24Z dmitriy $*/
+require_once('head_inc.php');
+    // do orientation
+    if ( isset($_GET) && is_array($_GET) && count($_GET) > 0 ) {
+        if (array_key_exists('dummy', $_GET)) {
+            $dummy = intval(trim($_GET['dummy']));
+            if ($dummy) {
+              $orientation = "rows";
+              setcookie('orientation', '', time() - 100000, $root_dir, $host, false, true);              
+            } else {
+              $orientation = "cols";
+              setcookie('orientation', $orientation, 1800000000, $root_dir, $host, false, true);              
+            }
+        }
+    }    
+require_once('html_head_inc.php');
+require_once('mysql_log.php');
+
+    $cur_page = 'threads.php';
+    $max_page = get_max_pages_expanded();
+    $work_page = ($max_page - $page) + 1;
+    $start_timestamp = time();
+?>
+<script src="js/jquery-1.10.2.min.js"></script>
+<script language="JavaScript" src="<?=autoversion('js/junk.js')?>"></script>
+<script language="JavaScript" src="<?=autoversion('js/autoload.js')?>"></script>
+<script language="JavaScript" src="js/threads_autoload.js"></script>
+<base target="bottom">
+<?php
+
+//print($user_id);
+
+
+if (!is_null($user_id) && $user_id != null) {
+
+    $colorquery = "SELECT color_ribbon, color_ribbon_background, color_icon_hover, color_group_border, color_topics_unread, color_topics_hover, color_topics_visited, color_titles from confa_users where id = " . $user_id;
+    $colorresult = mysql_query($colorquery);
+    if ($colorresult) {
+	$colorrow = mysql_fetch_assoc($colorresult);
+        $ribbonColor=$colorrow['color_ribbon'];
+        $ribbonBackground=$colorrow['color_ribbon_background'];  
+        $iconHover=$colorrow['color_icon_hover'];
+        $groupBorder=$colorrow['color_group_border'];
+              
+        $color_topics_unread= $colorrow['color_topics_unread'];
+        $color_topics_hover=$colorrow['color_topics_hover'];
+        $color_topics_visited=$colorrow['color_topics_visited'];
+        }
+     }
+     else{
+        $ribbonColor='white';
+	$ribbonBackground='#0080c0';  
+        $iconHover='#0090c0';
+        $groupBorder='#0090c0';
+                                   
+        $color_topics_unread= '#0000FF';
+        $color_topics_hover= '#FF0000';
+        $color_topics_visited='#0080c0';
+         }
+                                         
+?>
+                                         
+<style>
+     a:link    {color:<?=$color_topics_unread?>; font-family:Verdana,Arial; text-decoration:none;}
+     a:visited {color:<?=$color_topics_visited?>; font-family:Verdana,Arial; text-decoration:none;}
+     a:active  {color:<?=$color_topics_hover?>; font-family:Verdana,Arial; text-decoration:none;}
+     a:hover   {color:<?=$color_topics_hover?>; font-family:Verdana,Arial; text-decoration:underline;}
+     a.user_link {color:black;}
+     .selected  {background:#E0F1FF; /* Yellow = #FFFF99 */}
+     h1<---->{font-family:Verdana,Arial; font-size:36pt;}
+     h2<--->{font-family:Verdana,Arial; font-size:24pt; color:#0080c0;}
+     h3<--->{font-family:Verdana,Arial; font-size:14pt; color:#0080c0;}
+     /* Pager */
+     .pagination li.active span {
+         background-color: <?=$ribbonBackground?>;
+         border-color: <?=$ribbonBackground?> ;
+         color: <?=$ribbonColor?>;
+     }
+     .pagination li a:hover, .pagination .dropdown-visible a.dropdown-trigger, .nojs .pagination .dropdown-container:hover a.dropdown-trigger {
+         background-color: <?=$iconHover?>;
+         border-color: <?=$ribbonBackground?>;
+         color:  <?=$ribbonColor?>;
+     }
+</style>
+
+</head>
+<body id="threads_body">
+<?php
+function _pages_function($add_fluff=false) {
+    global $max_page, $page, $cur_page;
+    print('<a id="up" name="up"></a>');
+    print_pages($max_page, $page, 'contents', $cur_page, '', $add_fluff, $add_fluff);
+    // print_pages(5, $page, 'contents', $cur_page, '', $add_fluff, $add_fluff);
+}
+
+    $show_hidden = 2;
+    $ignored = array();
+
+    get_show_hidden_and_ignored();
+
+    _pages_function(false);  // to move to top, rename to "pages_function" and comment this call out
+    
+    print("<p/>");
+    
+    print('<div id="threads">');
+
+    $limit = strpos($agent, 'iPad') ? 7 : (strpos($agent, 'iPhone') || strpos($agent, 'like Mac OS') ? 5 : 200); 
+    
+    $result = get_threads_ex($limit);
+    $content = array();
+    $last_thread = -1;
+    $msgs = print_threads_ex($result, $content, $last_thread, $limit);
+    print_msgs($content, $msgs);
+
+    print('</div>');  
+    
+    // print_pages($max_page, $page, 'contents', $cur_page);
+    print('<BR><a href="#up" target="_top">Up</a>');
+    print('&nbsp;&nbsp;<a href="javascript:load_threads(document.getElementById(\'threads\'), '.$last_thread.','.$limit.');" target="_top">More</a>');
+
+    $end_timestamp = time();
+    $duration = $end_timestamp - $start_timestamp;
+    print('<!-- ' . $duration . ' milliseconds -->'); 
+
+   // print($show_hidden . "|");
+   // print_r($ignored);
+   autoload_threads($last_thread, $limit);
+?>
+<table cellpadding=1 cellspacing=0 width="90%">
+  <tr>
+    <td align="left">
+      <h4></h4>
+    </td>
+    <td align="right">
+      <h4></h4>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
+
+<?php
+
+require_once('tail_inc.php');
+
+?>
+
