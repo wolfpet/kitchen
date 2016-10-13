9f32b9acfd0fb7b2b8aa53517a46e8317846b1b5
wolfpet <p_wol@hotmail.com>
2016-10-13 20:45:22 +0000

    Bugfix: login.php forwards back to the top page now.

--- Forum/php/login.php
+++ Forum/php/login.php
@@ -1,9 +1,9 @@
 <?php
 /*$Id: login.php 379 2009-11-02 19:40:34Z dmitriy $*/
 
-  require_once('login_inc.php');
-  $lastpage = basename($lastpage);
-  // header("Location: $lastpage", TRUE, 302);
-  require_once($lastpage);
+    $new_pm = 0;
+    require_once('head_inc.php');
+    $logged_in = login($user, $password);
+    header("Location: top.php", TRUE, 302);
 ?>
 
