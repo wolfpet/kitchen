9ee8e3fff43952570678e48ce8b37d74fb1aa3f8
wolfpet <p_wol@hotmail.com>
2017-02-08 14:07:14 +0000

    added the bottom frame id for gallery support

--- Forum/php/top.php
+++ Forum/php/top.php
@@ -6,35 +6,23 @@ require_once('mysql_log.php');
 ?>
 <base target="bottom">
 <script type="text/javascript" src="js/threads_autoload.js"></script>
-<script>
-
-function setVerticaLayout()
-{
-    document.getElementById('frame1').style.float = 'left';
-    document.getElementById('frame1').style.width = '50vw';
-    document.getElementById('frame1').style.height = '90vh';
-    document.getElementById('frame2').style.float = 'right';
-    document.getElementById('frame2').style.width = '48vw';
-    document.getElementById('frame2').style.height = '90vh';
-    document.getElementById('hr1').style.display = 'none';            
-    //document.getElementById('hr2').style.display = 'block';            
-}
-</script>
+<script type="text/javascript" src="js/controls.js"></script>
 <title><?=$title?></title>
 </head>
 <body id="html_body" style="overflow: hidden;">
 <?php
 require('menu_inc.php');
-require_once('tail_inc.php');
 ?>
-
 <div id="frame1" style="position: static; height: 40vh; background-color: white;display: inline-block;width: 100vw;">
     <iframe style="border: none;" width="100%" height="100%" name="contents" src="threads.php"></iframe>
 </div>
-<div id="hr1"></div>
+<hr id="hr1">
 <div id="frame2" style="position: relative;height: 48vh; background-color: white;display: inline-block; width: 100vw;">
-    <iframe style="border: none;" width="100%" height="100%" name="bottom" src="welc.php"></iframe>
+    <iframe style="border: none;" width="100%" height="100%" name="bottom" id="bottom" src="welc.php"></iframe>
 </div>
-<div style="display:none;" id="hr2"></div>
+<?php
+require('gallery_inc.php');
+require_once('tail_inc.php');
+?>
 </body>
 </html>
