3506dec79093199375e58e85dd18b5c8cd373a87
wolfpet <p_wol@hotmail.com>
2017-02-08 14:10:20 +0000

    added PhotoGallery inc and api

--- Forum/php/gallery_inc.php
+++ Forum/php/gallery_inc.php
@@ -1,25 +1,26 @@
 <?php
-
+ 
 ?>
 <style>
 div.gallery_container {
-    width: 100%;
+    width: 100vw;
     border: 0px solid gray;
     position: absolute;
     top: 53px;
     background-color: black;
-    max-height: 1000px;
+    max-height: 100vh;
     height: 100vh;
     display: none;
 }
         
 header {
     margin-top: 3vh;
-    margin-left: 6%;
+    margin-left: 0%;
     padding: 0px;
     color: grey;
     background-color: black;
     clear: left;
+    text-align: center;
     font-size: 8pt;
 }
 
@@ -52,26 +53,98 @@ div.gallery_exit_btn_div:hover #gallery_exit_btn { fill: #fff; }
 img {
     background: black;
     vertical-align: middle;
-    max-width: 1000px;
-    max-height: 700px;
+    max-height: 80vh;
+    max-width: 88vw;
 }
 </style>
 
+<script>
+var currentPhotoIndex=0;
+var currentAuthor=0;
+var currentMsg=0;
+var galleryImages = [];
+
+function initGalleryImages()
+{
+    currentPhotoIndex=0;
+    document.getElementById("gallery_title").text = "Photo Gallery: images from all users";
+    $.post("gallery_api.php", {'get_imgUrls_and_Posts' : 'all'}, function(data,status)
+    {
+        galleryImages = $.map(data, function(value, index) {
+	return [value];
+    });
+	document.getElementById("currentPhoto").src = galleryImages[currentPhotoIndex][0];
+	document.getElementById("gallery_author_name").text = galleryImages[currentPhotoIndex][1]; 
+	document.getElementById("gallery_msg_title").text = galleryImages[currentPhotoIndex][2]; 
+	currentMsg = galleryImages[currentPhotoIndex][3];
+	currentAuthor = galleryImages[currentPhotoIndex][4];
+    });
+    
+        
+}
+
+function nextPhoto()
+{
+    if((currentPhotoIndex + 2) < galleryImages.length)
+    {
+        currentPhotoIndex++;
+	document.getElementById("currentPhoto").src = galleryImages[currentPhotoIndex][0];
+	document.getElementById("gallery_author_name").text = galleryImages[currentPhotoIndex][1]; 
+	document.getElementById("gallery_msg_title").text = galleryImages[currentPhotoIndex][2]; 
+	currentMsg = galleryImages[currentPhotoIndex][3];
+	currentAuthor = galleryImages[currentPhotoIndex][4];
+
+    }
+}
+
+function prevPhoto()
+{
+    if((currentPhotoIndex - 1) < 0)return;
+        currentPhotoIndex--;
+        document.getElementById("currentPhoto").src = galleryImages[currentPhotoIndex][0];
+	document.getElementById("gallery_author_name").text = galleryImages[currentPhotoIndex][1]; 
+	document.getElementById("gallery_msg_title").text = galleryImages[currentPhotoIndex][2]; 
+	currentMsg = galleryImages[currentPhotoIndex][3];
+	currentAuthor = galleryImages[currentPhotoIndex][4];
+
+}
+
+function filterByName(userId)
+{
+    document.getElementById("gallery_title").text = galleryImages[currentPhotoIndex][1] + "'s Photo Gallery. ";
+    $.post("gallery_api.php", {'get_imgUrls_and_Posts' : userId}, function(data,status)
+    {
+        galleryImages = $.map(data, function(value, index) {
+	return [value];
+    });
+    currentPhotoIndex=0;
+    document.getElementById("currentPhoto").src = galleryImages[currentPhotoIndex][0];
+    });
+}
+
+function openMsg(msgId)
+{
+    closeGallery();
+    document.getElementById("bottom").src="msg.php?id=" + currentMsg;
+}
+
+</script>
+
 <div id="menu_cover" style="width: 100vw;position: absolute;top: 0px;color: white; background-color: black; opacity: 0.6;height: 53px;color: white; display: none"></div>
 <div class="gallery_container" id="gallery">
-     <header>Photo Gallery</header>
+     <header><a id="gallery_title">Photo Gallery: images from all users</a></header>
      <div>
 	<div class="column-center">
-	    <img src="https://teslamotorsclub.com/tmc/attachments/img_20161003_113644-1-jpg.197986/" style="
-		width: 100%;
+	    <img id="currentPhoto" src=""
+		style="
 		margin: auto;
 		display: block;
 	    ">
 	</div>
-	<div class="column-left"><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g><path id="gallery_nav_left" fill="#444" d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></g></svg></div>
-	<div class="column-right"><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g><path id="gallery_nav_right" fill="#444" d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></g></svg></div>
+	<div class="column-left" onclick="prevPhoto();"><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g><path id="gallery_nav_left" fill="#444" d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></g></svg></div>
+	<div class="column-right" onclick="nextPhoto();";><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g><path id="gallery_nav_right" fill="#444" d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></g></svg></div>
 	</div>
-    <footer>Post title and the user name goes here</footer>
+    <footer ><a style="cursor: pointer"id="gallery_msg_title" onclick="openMsg(currentMsg)">title</a> by <a style="cursor: pointer" id="gallery_author_name" onclick="filterByName(currentAuthor);">user</a></footer>
 
 <div id="gallery_menu" style="width: 100vw;position: absolute;top: 0px;color: white; opacity: 1.0;height: 53px;color: white; display: block">
     <div class="gallery_exit_btn_div" onclick="closeGallery();" style="width: 30px; margin-right: 8px; margin-top: 8px; float: right;cursor: pointer;">
