a052f25039c055cfdad0eaa0684d6a685b3a3983
root <p_wol@hotmail.com>
2018-03-21 20:49:31 +0000

    movies and books mod delete

--- Forum/php/books.php
+++ Forum/php/books.php
@@ -5,7 +5,21 @@ require_once('html_head_inc.php');
 require_once('custom_colors_inc.php');
 
 ?>
-
+<script>
+function delBook(msgid)
+{
+  var user_id = <?php print($user_id);  ?>;
+      var message_id=msgid;
+    //add the msgid to movies
+      $.ajax({
+            type: "POST",
+      url: "books_del.php",
+      data: {user: user_id, msg: msgid} ,
+      success: function(data){
+      }
+    });
+}
+</script>
 <h3>Books</h3>
 
 <dl>
@@ -16,7 +30,10 @@ $result = mysql_query($query);
 if (!$result) {die('Query failed ');}
 while ($row = mysql_fetch_assoc($result))
 {
- print('<img border="0" src="images/es.gif" width="16" height="16" align="top" style="padding:0px 0px 3px 0px;">&nbsp;<a href="msg.php?id='.$row['msgid'].'" target="bottom">'.$row['subject'].'</a>&nbsp;<b>'.$row['username'].'</b><br>');
+ print('<img border="0" src="images/es.gif" width="16" height="16" align="top" style="padding:0px 0px 3px 0px;">&nbsp;<a href="msg.php?id='.$row['msgid'].'" target="bottom">'.$row['subject'].'</a>&nbsp;<b>'.$row['username'].'</b>');
+ if ( !is_null( $moder ) && $moder > 0){print(' <a href="books.php" style="cursor: pointer" onclick="delBook('.$row['msgid'].')">Hide</a>');}
+ print('<BR>');
+  
 }
 
 
--- Forum/php/movies.php
+++ Forum/php/movies.php
@@ -5,6 +5,21 @@ require_once('html_head_inc.php');
 require_once('custom_colors_inc.php');
 
 ?>
+<script>
+function delMovie(msgid)
+{
+  var user_id = <?php print($user_id);  ?>;
+    var message_id=msgid;
+      //add the msgid to movies
+      $.ajax({
+              type: "POST",
+              url: "movies_del.php",
+              data: {user: user_id, msg: msgid} ,
+              success: function(data) {
+            }
+      });
+}
+</script>
 
 <h3>Movies</h3>
 
@@ -16,8 +31,11 @@ $result = mysql_query($query);
 if (!$result) {die('Query failed ');}
 while ($row = mysql_fetch_assoc($result))
 {
- print('<img border="0" src="images/youtube12x12w.png" width="16" height="16" align="top" style="padding:0px 0px 3px 0px;">&nbsp;<a href="msg.php?id='.$row['msgid'].'" target="bottom">'.$row['subject'].'</a>&nbsp;<b>'.$row['username'].'</b><br>');
+ print('<img border="0" src="images/youtube12x12w.png" width="16" height="16" align="top" style="padding:0px 0px 3px 0px;">&nbsp;<a href="msg.php?id='.$row['msgid'].'" target="bottom">'.$row['subject'].'</a>&nbsp;<b>'.$row['username'].'</b>');
+ if ( !is_null( $moder ) && $moder > 0){print('<a href="movies.php" style="cursor: pointer" onclick="delMovie('.$row['msgid'].')">&nbsp;Hide</a>');}
+ print('<BR>');
 }
+
 
 
 ?>
