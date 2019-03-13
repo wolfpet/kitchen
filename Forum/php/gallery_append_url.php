51c49d1a0f0a77828ce54016f690aa5796424f43
iamranger <iamranger@users.noreply.github.com>
2019-03-12 21:55:11 -0700

    fixed spaces in names of uploaded files

--- Forum/php/gallery_append_url.php
+++ Forum/php/gallery_append_url.php
@@ -1,6 +1,11 @@
 <html>
 <script>
-parent.insertBodyText('body','<?=$_GET["image"]?>');
+<?php
+  $exploded = explode("//", $_GET["image"]);
+?>
+parent.insertBodyText('body','<?=implode("//", array(
+      $exploded[0], 
+      implode("/", array_map("rawurlencode", explode("/", $exploded[1]))) ))?>');
 parent.toggleImageUpload();
 </script>
 </html>
