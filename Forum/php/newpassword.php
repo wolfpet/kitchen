99e1c8abee48537c6c7c72cfa92c308f2bb761b8
iamranger <iamranger@users.noreply.github.com>
2024-11-12 22:50:50 -0800

    fixes GMail integration

--- Forum/php/newpassword.php
+++ Forum/php/newpassword.php
@@ -39,7 +39,7 @@ require_once('html_head_inc.php');
                 $message = $user . ", your new generated password is\n\n" . $newpass . "\n\nFeel free to login and change it in your profile";
                 $from = $from_email;
                 $headers = "From: $from";
-                if (mail($to,$subject,$message,$headers)) {
+                if (xmail($to,$subject,$message,$headers)) {
                   $query = 'UPDATE confa_users set password=password(\'' . $newpass . '\'), modified=NULL  where username=\'' . mysql_real_escape_string($user) . '\'';
                   $result = mysql_query($query);
                   if (!$result) {
--- Forum/php/reg.php
+++ Forum/php/reg.php
@@ -158,8 +158,8 @@ if (isset($recaptcha_site_key) && isset($recaptcha_secret_key)) {
         if ( !isset($reg_type) || $reg_type == REG_TYPE_OPEN )
           print($message);
         else if ( isset($reg_type) && $reg_type == REG_TYPE_EMAIL ) {
-          mail($to,$subject,$message,$headers);
-          print("<B>" . $user . "</B>, activation link has been sent to " . htmlentities($email, HTML_ENTITIES,'UTF-8') . ". The link will be valid for 86400 seconds ( 24 hours )");
+          xmail($to,$subject,$message,$headers);
+          print("<B>" . $user . "</B>, activation link has been sent to " . htmlentities($email, HTML_ENTITIES,'UTF-8') . ". The link will be valid for 24 hours");
         } else if ( isset($reg_type) && $reg_type == REG_TYPE_CONFIRM ) {
           print("In order to activate your account, your request needs to be approved by a moderator. Until that time, feel free to read the forum.");
         } else
